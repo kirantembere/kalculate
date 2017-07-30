41a52c3c6af32ff3f3a1847e4d433fee961eda30
kirantembere <kirantembere@gmail.com>
2017-07-29 22:08:05 -0400

    added dev dependencies and basic angular template

---
+++ app/index.js
@@ -0,0 +1,4 @@
+﻿angular.module('kalculator', [])
+    .controller('mainCtrl', function ($scope) {
+        $scope.pageHeader = "This is a calculator app";
+    });
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+
 app.use(express.static(__dirname + '/app')); //__dir and not _dir
 var port = 8000; // you can use any port
 app.listen(port);
