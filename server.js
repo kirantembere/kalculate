5fe83b3346641aec426c6c058c938b88019b8344
kirantembere <kirantembere@gmail.com>
2017-08-11 23:59:03 -0400

    organized the folder structure

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/app')); //__dir and not _dir
+app.use(express.static(__dirname + '/app/emi')); //__dir and not _dir
 var port = 8000; // you can use any port
 app.listen(port);
 console.log('server on' + port);
